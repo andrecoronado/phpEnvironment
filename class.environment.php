67f5544a7038f14b6f0aa76f01897411561ef341
Andre Coronado <54828512+andrecoronado@users.noreply.github.com>
2019-12-03 18:09:43 -0500

    New features
    Add 4 functions: dirToArray, diffFiles, exportFilesTo, importFilesFrom.
    Now is possible copy (import/export) all files from one Environment to another.

--- class.environment.php
+++ class.environment.php
@@ -7,6 +7,7 @@ class Environment {
     private $dbIP;
     private $dbUser;
     private $dbPass;
+    private $arrayPath=array();
     public $path;
     public $erro;
     public $name;
@@ -29,6 +30,8 @@ class Environment {
                 $this->color_fundo = $registro->color_fundo;
                 $this->color_menu = $registro->color_menu;
             }
+            $temp=Environment::dirToArray($GLOBALS['ROOT_PATH'] . '/' . $registro->page);
+            $this->arrayPath[$registro->page]=$temp;
         } 
       }
       
@@ -50,4 +53,116 @@ class Environment {
         }
         return ($consction);
     }
+    private function dirToArray($dir) {
+  
+        $result = array();
+        $arrayDir = scandir($dir);
+        foreach ($arrayDir as $key => $value)
+        {
+           if (!in_array($value,array(".","..")))
+           {
+              if (is_dir($dir . '/' . $value))
+              {
+                 $result=array_merge($result,Environment::dirToArray($dir . '/' . $value));
+              }
+              else
+              {
+                 array_push($result, $dir . '/' . $value);
+              }
+           }
+        }
+        return $result;
+    }
+
+    public function diffFiles($page) {
+        foreach ($this->arrayPath as $key => $value)
+        {
+            if($key==$this->page){
+                $arrMain=$value;
+            }
+            elseif ($key==$page) {
+                $arrSecond=$value;
+            } 
+        }
+        $result=array();
+        $modFile=array();
+        $newFile=array();
+        $delFile=array();
+        foreach ($arrMain as $value)
+        {
+            $newPathFile = str_replace($this->page, $page, $value);
+            if(in_array($newPathFile,$arrSecond)){
+                $tempThisFile = file_get_contents($value);
+                $tempNewFile = file_get_contents($newPathFile);
+                if($tempThisFile<>$tempNewFile)
+                {
+                    array_push($modFile,$value);
+                }
+            }
+            else 
+            {
+                array_push($newFile,$value);
+            }
+        }
+        //check if this any file was delete
+        foreach ($arrSecond as $value)
+        {
+            $newPathFile = str_replace($page, $this->page, $value);
+            if(!in_array($newPathFile,$arrMain)){
+                array_push($delFile,$value);
+            }
+        }
+        $result['newFile']=$newFile;
+        $result['modFile']=$modFile;
+        $result['delFile']=$delFile;
+
+        return $result;
+    }
+
+    public function exportFilesTo($page) {  //Copy files from this local path to $page path
+        foreach ($this->arrayPath as $key => $value)
+        {
+            if($key==$this->page){
+                $arrMain=$value;
+            }
+        } 
+        $temp=array();
+        foreach ($arrMain as $value)
+        {
+            $newPathFile = str_replace($this->page, $page, $value);
+            if(!is_dir (dirname($newPathFile))){
+                mkdir(dirname($newPathFile), 0777, true);
+            }
+            $tempFile=file_get_contents($value);
+            file_put_contents($newPathFile,$tempFile);
+
+            array_push($temp,$newPathFile);            
+        }
+        if(in_array($page,$this->arrayPath))
+        {
+            array_push($this->arrayPath,$page,$temp);
+        }  
+        return $temp;
+    }   
+    
+    public function importFilesFrom($page) {  //Copy files from $page local path to this local path
+        foreach ($this->arrayPath as $key => $value)
+        {
+            if($key==$page){
+                $arrMain=$value;
+            }
+        } 
+        $temp=array();
+        foreach ($arrMain as $value)
+        {
+            $newPathFile = str_replace($page, $this->page, $value);
+            if(!is_dir (dirname($newPathFile))){
+                mkdir(dirname($newPathFile), 0777, true);
+            }
+            $tempFile=file_get_contents($value);
+            file_put_contents($newPathFile,$tempFile);   
+            array_push($temp,$newPathFile);    
+        }
+        return $temp;
+    }         
 }
