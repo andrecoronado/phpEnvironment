e4c01956e8e1eaa338bc1eeda48b56627ac5d357
Andre Coronado <54828512+andrecoronado@users.noreply.github.com>
2019-12-02 23:04:39 -0500

    Update config.php

--- config.php
+++ config.php
@@ -15,16 +15,8 @@ $GLOBALS['colorFundo']=$environment->color_fundo;
 if($environment->color_menu=="default"){$GLOBALS['colorMenu']="";}
 else{$GLOBALS['colorMenu']='style="background-color:'.$ambiente->color_menu.';"';}
 
-//display_errors
-if($Environment->erro === true)
-{
-    error_reporting(E_ALL^E_WARNING^E_NOTICE);
-    ini_set(“display_errors”, 1 );
-}
-else 
-{
-    error_reporting(0);
-    ini_set(“display_errors”, 0 );
-}
+$conection=$environment->conectDB());
+
+
 
 
